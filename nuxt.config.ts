4fd55d28d2cd5f44163565a7c5b2747eccb19ea9
อิลฮัม หะยีดอเล๊าะ <ihsaniah112@gmail.com>
2026-08-15 00:22:20 +0700

    chore: add node types and configure vite server options and experimental app manifest settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,17 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  experimental: {
+    appManifest: false,
+  },
+  vite: {
+    server: {
+      allowedHosts: true,
+      fs: {
+        strict: false,
+      },
+    },
+  },
   runtimeConfig: {
     cronSecret: '',
     jwt: {
